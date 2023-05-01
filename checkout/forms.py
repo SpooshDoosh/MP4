82093d1af8a52183caa7d06aba87cda8b5dd9167
SpooshDoosh <kelvinwnicholson@gmail.com>
2023-05-01 09:09:14 +0000

    Added forms and signals

---
+++ checkout/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    model = Order
+    fields = ('full_name', 'email', 'phone_number',
+              'street_address1', 'street_address2',
+              'town_or_city', 'postcode', 'country',
+              'county',)
+
+    def __innit__(self, *args, **kwargs):
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Contact Number',
+            'street_address1': '1st Line of Street Address',
+            'street_address2': '2nd Line of Street Address',
+            'town_or_city': 'Town or City',
+            'county': 'County',
+            'postcode': 'Postcode',
+            'country': 'Country',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
