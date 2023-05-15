ee041510fbf9ff95179ab3f53c6d9a994fe0318b
SpooshDoosh <kelvinwnicholson@gmail.com>
2023-05-15 13:32:37 +0000

    Install decouple for secret key

--- white_feather/settings.py
+++ white_feather/settings.py
@@ -1,8 +1,8 @@
 import os
 import dj_database_url
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
+from decouple import config
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVLEOPMENT' in os.environ
