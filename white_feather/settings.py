4dbfb3125b6aaa73e89b606fa602d109a1594695
SpooshDoosh <kelvinwnicholson@gmail.com>
2023-05-20 07:15:29 +0000

    Add forward slash to MEDIA_URL

--- white_feather/settings.py
+++ white_feather/settings.py
@@ -165,7 +165,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
