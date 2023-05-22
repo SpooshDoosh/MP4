1c75bfc238988c9687a013f22c9ebaa5016bdb56
SpooshDoosh <kelvinwnicholson@gmail.com>
2023-05-22 09:12:04 +0000

    Add view for musicplayer and added song to media

--- musicplayer/views.py
+++ musicplayer/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import MusicPlayer
 
 # Create your views here.
+
+
+class RenderHTMLPlayer(TemplateView):
+    template_name = 'music_player.html'
+    model = MusicPlayer
+
+    def get(self, request, *args, **kwargs):
+        id = self.kwargs.get('id')
+        song = MusicPlayer.objects.filter(id=id).first()
+        song_uri = '/media' + \
+            request.build_absolute_uri(podcast.file.url).split('media')[1]
+
+        context = {'track_url': track_uri}
+        return render(request, self.template_name, context)
